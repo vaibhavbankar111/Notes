a1a858525fc649e8462bae11a062216040835979
hackerboypk <43108239+hackerboypk@users.noreply.github.com>
2023-01-17 13:32:29 +0530

    Update terra_tasks.tf

--- terra_tasks.tf
+++ terra_tasks.tf
@@ -461,3 +461,90 @@ variable "instance_type" {
 variable "aws_region" {
   default = "eu-north-1"
 }
+======================================
+user data script
+	main.tf
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "jenkins" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = var.instance_type
+  associate_public_ip_address = true
+  key_name = var.key_name
+  vpc_security_group_ids = [aws_security_group.jenkins_sg.id]
+  subnet_id = var.subnet_id
+  user_data = <<EOF
+  #!/bin/bash
+sudo apt update -y
+sudo apt install default-jre -y
+sudo apt install nfs-common -y
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt update -y
+mkdir -p /var/lib/jenkins
+sudo apt install jenkins -y
+sudo chown -R jenkins:jenkins /var/lib/jenkins
+sudo systemctl start jenkins
+  EOF
+
+  tags = {
+    Name = "jenkins-instance"
+  }
+}
+resource "aws_security_group" "jenkins_sg" {
+  name        = "jenkins-sg"
+  description = "for jenkins"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 8080
+    to_port          = 8080
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
+resource "tls_private_key" "jen" {
+  algorithm = "RSA"
+  rsa_bits = "4096"
+  
+}
+
+# resource "aws_key_pair" "jenkins-key" {
+#   key_name = "jenkins-key"
+#   public_key = tls_private_key.jen
+  
+# }
+
